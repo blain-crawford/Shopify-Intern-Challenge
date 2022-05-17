95b7e86db30b6d8193f9e3c67432394f025d28bd
Blain Crawford <blaincrawford@gmail.com>
2022-05-17 15:38:48 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+document.title = 'LyricGenerator';
+
+ReactDOM.render(<App />, document.getElementById('app'));
