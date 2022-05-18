ccc2e1269b35679a0da2f51d65b7857a5a740155
Blain Crawford <blaincrawford@gmail.com>
2022-05-18 07:39:40 -0400

    finishing app and about to try some hosts

--- src/components/ApiResponses.js
+++ src/components/ApiResponses.js
@@ -9,7 +9,7 @@ import {
   StyledCardButton,
   StyledLyricList,
   StyledTextField,
-} from '../components/mui-styles/apiResponseStyles';
+} from './mui-styles/apiResponseStyles';
 
 const ApiResponses = ({
   suggestedLyrics,
